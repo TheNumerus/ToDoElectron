44a04b8b006d1b1e40fd625b4aacc8cdc615f93a
TheNumerus <Petr.volf216@gmail.com>
2017-10-08 21:48:54 +0200

    bump version number

--- src/globalProperties.js
+++ src/globalProperties.js
@@ -1,5 +1,5 @@
 const Properties = {
-	appVersion: '0.0.1',
+	appVersion: '0.0.2',
 	appName: 'ToDoElectron',
 	trelloAppKey: '01ad9ee9ec7a92b20ddd261ff55820f4',
 	trelloSecretKey: '7b455f5b12ca3b432bb34c381e00b594b53adca7fc5789449a1569f59ab2449c',
